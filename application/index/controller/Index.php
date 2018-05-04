a32fd405c520bb1a7e6ee3271e689992cfcdf01f
yuanyuanyuan <turing_zhy@163.com>
2018-05-04 23:23:50 +0800

    needs

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -11,8 +11,24 @@ class Index
     //@desn:接收需求
     //@param:
     public function add_needs(){
-        echo 'test';
+        $aa[] = $need_name = input('param.need_name/s');
+        $aa[] = $e_mail = Request::instance()->param('e_mail','',FILTER_VALIDATE_EMAIL);
+        $aa[] = $phone = input('param.phone/s');
+        $aa[] = $company_name = input('param.company_name/s');
+        $aa[] = $needs_desc = input('param.needs_desc/s');
+
+        var_dump($aa);
     }
+
+    /*create table hw_needs (
+    id int primary key auto_increment comment 'needs_id',
+    needs_name varchar(255) comment 'costomer_name',
+    e_mail varchar(255) not null comment 'needs_user_email',
+    phone varchar(255) not null comment 'needs_costomer_phone',
+    company_name varchar(255) not null comment 'needs_company',
+    needs_desc varchar(1000) not null comment 'needs_desc',
+    add_time int not null comment 'add_time'
+    )engine=innodb charset = utf8 comment 'needs_table';*/
 
 
 }
