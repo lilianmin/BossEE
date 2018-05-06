11fafea0a1605eebd5e45559532b3852ef900e9b
yuanyuanyuan <turing_zhy@163.com>
2018-05-06 18:03:01 +0800

    apidoc

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,4 +1,4 @@
-{
+define({
   "name": "example",
   "version": "0.1.0",
   "description": "apiDoc basic example",
@@ -9,8 +9,8 @@
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-06T10:00:09.564Z",
+    "time": "2018-05-06T10:02:16.253Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
-}
+});
