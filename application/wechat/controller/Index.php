a7bcf31b0777befdd306cdca82bf02e84efa5f68
yuanyuanyuan <turing_zhy@163.com>
2018-06-03 14:02:13 +0800

    wechat

--- application/wechat/controller/Index.php
+++ application/wechat/controller/Index.php
@@ -1,15 +1,10 @@
 <?php
-//namespace app\wechat\controller;
+namespace app\wechat\controller;
 use tool\GetPublic;
 use wechat\crypt;
 
 class Index
 {
-    public function index()
-    {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
-    }
-
     /*
      * @desn:配置公众号菜单
      * @date:2018-01-01
@@ -38,60 +33,141 @@ class Index
     public function access_to_wechat(){
         //获取微信配置
         list($AppID,$AppSecret,$token,$encodingAesKey) = GetPublic::get_wechat_config();
-        $wechat = new crypt($token, $encodingAesKey, $AppID);
-        //接入服务器验证
-        //$wechat->checkSignature();
-
-        //include_once "wxBizMsgCrypt.php";
-
-        // 第三方发送消息给公众平台
-        $timeStamp = time();
-        $nonce = "xxxxxx";
-        $text = "<xml>
-                    <ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName>
-                    <FromUserName><![CDATA[gh_7f083739789a]]></FromUserName>
-                    <CreateTime>1407743423</CreateTime>
-                    <MsgType><![CDATA[video]]></MsgType>
-                    <Video>
-                    <MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId>
-                    <Title><![CDATA[testCallBackReplyVideo]]></Title>
-                    <Description><![CDATA[testCallBackReplyVideo]]></Description>
-                    </Video>
-                </xml>";
-
-
-        //$pc = new WXBizMsgCrypt($token, $encodingAesKey, $appId);
-        $encryptMsg = '11111';
-        $errCode = $wechat->encryptMsg($text, $timeStamp, $nonce, $encryptMsg);
-        if ($errCode == 0) {
-            print("加密后: " . $encryptMsg . "\n");
-        } else {
-            print($errCode . "\n");
-        }
-
-        $xml_tree = new DOMDocument();
-        $xml_tree->loadXML($encryptMsg);
-        $array_e = $xml_tree->getElementsByTagName('Encrypt');
-        $array_s = $xml_tree->getElementsByTagName('MsgSignature');
-        $encrypt = $array_e->item(0)->nodeValue;
-        $msg_sign = $array_s->item(0)->nodeValue;
-
-        $format = "<xml>
-                        <ToUserName><![CDATA[toUser]]></ToUserName>
-                        <Encrypt><![CDATA[%s]]></Encrypt>
-                    </xml>";
-        $from_xml = sprintf($format, $encrypt);
-
-        // 第三方收到公众号平台发送的消息
-        $msg = '11111';
-        $errCode = $wechat->decryptMsg($msg_sign, $timeStamp, $nonce, $from_xml, $msg);
-        if ($errCode == 0) {
-            print("解密后: " . $msg . "\n");
-        } else {
-            print($errCode . "\n");
-        }
+        $this->echoStr($token);
     }
     public function test(){
         echo 11111111111;
+    }
+
+
+    public function index()
+    {
+        $this->show('This is for Wechat','utf-8');
+    }
+    //用户首次配置开发环境
+    //@param:$token  token
+    public function echoStr($token)
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce     = $_GET["nonce"];
+        $echostr   = $_GET['echostr'];
+        $token     = 'skye';
+        $tmpArr    = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr    = implode( $tmpArr );
+        $tmpStr    = sha1( $tmpStr );
+        if( $tmpStr == $signature && $echostr)
+        {
+            echo $echostr;
+        }else{
+            $this->reposeMsg();
+        }
+    }
+
+    //回复消息
+    public function reposeMsg()
+    {
+        //1.接受数据
+        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];  //接受xml数据
+        //2.处理消息类型,推送消息
+        $postObj = simplexml_load_string( $postArr );   //将xml数据转化为对象
+        if( strtolower( $postObj->MsgType ) == 'event')
+        {
+            //关注公众号事件
+            if( strtolower( $postObj->Event ) == 'subscribe' )
+            {
+                $content   =  '你终于来啦,等你等的好辛苦啊!可尝试输入关键字:教程,Tel,wechat,1等000';
+                $this->subscribe($postObj,$content);  //关注发送文本消息
+            }
+        }
+
+        //回复文本信息
+        if( strtolower( $postObj->MsgType ) == 'text' && trim($postObj->Content)=='wechat')
+        {
+            $toUser = $postObj->FromUserName;
+            $fromUser = $postObj->ToUserName;
+            $arr = array(
+                array(
+                    'title'=>'test',
+                    'description'=>"just so so...",
+                    'picUrl'=>'http://www.acting-man.com/blog/media/2014/11/secret-.jpg',
+                    'url'=>'http://www.imooc.com',
+                ),
+                array(
+                    'title'=>'hao123',
+                    'description'=>"hao123 is very cool",
+                    'picUrl'=>'https://www.baidu.com/img/bdlogo.png',
+                    'url'=>'http://www.hao123.com',
+                ),
+                array(
+                    'title'=>'qq',
+                    'description'=>"qq is very cool",
+                    'picUrl'=>'http://www.imooc.com/static/img/common/logo.png',
+                    'url'=>'http://www.qq.com',
+                ),
+            );
+            $template = "<xml>  
+                 <ToUserName><![CDATA[%s]]></ToUserName>  
+                 <FromUserName><![CDATA[%s]]></FromUserName>  
+                 <CreateTime>%s</CreateTime>  
+                 <MsgType><![CDATA[%s]]></MsgType>  
+                 <ArticleCount>".count($arr)."</ArticleCount>  
+                 <Articles>";
+            foreach($arr as $k=>$v){
+                $template .="<item>  
+                    <Title><![CDATA[".$v['title']."]]></Title>   
+                    <Description><![CDATA[".$v['description']."]]></Description>  
+                    <PicUrl><![CDATA[".$v['picUrl']."]]></PicUrl>  
+                    <Url><![CDATA[".$v['url']."]]></Url>  
+                    </item>";
+            }
+
+            $template .="</Articles>  
+                 </xml> ";
+            echo sprintf($template, $toUser, $fromUser, time(), 'news');
+            //注意：进行多图文发送时，子图文个数不能超过10个
+        }else{
+            switch( trim( $postObj->Content ) )
+            {
+                case 1:
+                    $content = '你输入了个数字1';
+                    break;
+                case Tel:
+                    $content = '12345678901';
+                    break;
+                case '教程':
+                    $content = "<a href='www.imooc.com'>慕课网</a>";
+                    break;
+                case '博客':
+                    $content = "<a href='blog.abc.com'>测试微信</a>";
+                    break;
+                default:
+                    $content = '升级打造中...';
+                    break;
+            }
+            $this->wechat_return_text($postObj,$content);
+        }
+    }
+
+    /*
+     * 回复文本信息
+     * @param:wechat服务器返回消息对象
+     * @param:$content  回复文本信息
+     * @return:返回xml数据
+     */
+    private function wechat_return_text($postObj,$content){
+        $toUser    =  $postObj->FromUserName;
+        $fromUser  =  $postObj->ToUserName;
+        $time      =  time();
+        $msgType   =  'text';
+        $template  =  "<xml>  
+                    <ToUserName><![CDATA[%s]]></ToUserName>  
+                    <FromUserName><![CDATA[%s]]></FromUserName>  
+                    <CreateTime>%s</CreateTime>  
+                    <MsgType><![CDATA[%s]]></MsgType>  
+                    <Content><![CDATA[%s]]></Content>  
+                    </xml>";
+        echo sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
     }
 }
