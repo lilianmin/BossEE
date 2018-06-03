3ae32ad0058da2d1c4f6c88455d7355582bcb594
yuanyuanyuan <turing_zhy@163.com>
2018-06-03 12:53:25 +0800

    wechat

--- application/wechat/controller/Index.php
+++ application/wechat/controller/Index.php
@@ -39,7 +39,57 @@ class Index
         //获取微信配置
         list($AppID,$AppSecret,$token,$encodingAesKey) = GetPublic::get_wechat_config();
         $wechat = new crypt($token, $encodingAesKey, $AppID);
-        $wechat->checkSignature();
+        //接入服务器验证
+        //$wechat->checkSignature();
+
+        include_once "wxBizMsgCrypt.php";
+
+        // 第三方发送消息给公众平台
+        $timeStamp = time();
+        $nonce = "xxxxxx";
+        $text = "<xml>
+                    <ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName>
+                    <FromUserName><![CDATA[gh_7f083739789a]]></FromUserName>
+                    <CreateTime>1407743423</CreateTime>
+                    <MsgType><![CDATA[video]]></MsgType>
+                    <Video>
+                    <MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId>
+                    <Title><![CDATA[testCallBackReplyVideo]]></Title>
+                    <Description><![CDATA[testCallBackReplyVideo]]></Description>
+                    </Video>
+                </xml>";
+
+
+        //$pc = new WXBizMsgCrypt($token, $encodingAesKey, $appId);
+        $encryptMsg = '11111';
+        $errCode = $wechat->encryptMsg($text, $timeStamp, $nonce, $encryptMsg);
+        if ($errCode == 0) {
+            print("加密后: " . $encryptMsg . "\n");
+        } else {
+            print($errCode . "\n");
+        }
+
+        $xml_tree = new DOMDocument();
+        $xml_tree->loadXML($encryptMsg);
+        $array_e = $xml_tree->getElementsByTagName('Encrypt');
+        $array_s = $xml_tree->getElementsByTagName('MsgSignature');
+        $encrypt = $array_e->item(0)->nodeValue;
+        $msg_sign = $array_s->item(0)->nodeValue;
+
+        $format = "<xml>
+                        <ToUserName><![CDATA[toUser]]></ToUserName>
+                        <Encrypt><![CDATA[%s]]></Encrypt>
+                    </xml>";
+        $from_xml = sprintf($format, $encrypt);
+
+        // 第三方收到公众号平台发送的消息
+        $msg = '11111';
+        $errCode = $wechat->decryptMsg($msg_sign, $timeStamp, $nonce, $from_xml, $msg);
+        if ($errCode == 0) {
+            print("解密后: " . $msg . "\n");
+        } else {
+            print($errCode . "\n");
+        }
     }
     public function test(){
         echo 11111111111;
